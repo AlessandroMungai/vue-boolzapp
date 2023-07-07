183571ad5938ae7bbf4b78c2c5f72427d63af926
AlessandroMungai <mungai.alessandro90@gmail.com>
2023-07-07 12:40:36 +0200

    added dynamic messages

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const { createApp } = Vue;
 createApp ({
     data() {
         return {
+            
+            count: 0,
+
             contacts: [
                 {
                     name: 'Teo',
