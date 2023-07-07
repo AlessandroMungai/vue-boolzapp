735cd006c15061a2773d00f03f7e25a2b0260967
AlessandroMungai <mungai.alessandro90@gmail.com>
2023-07-07 09:17:06 +0200

    added v-for to stamp contacts

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ createApp ({
         return {
             contacts: [
                 {
-                    name: 'Michele',
-                    avatar: './img/avatar_1.png',
+                    name: 'Teo',
+                    avatar: './img/avatar_1.jpg',
                     visible: true,
                     messages:   
                     [
@@ -33,7 +33,7 @@ createApp ({
 
                 {
                     name: 'Fabio',
-                    avatar: './img/avatar_2.png',
+                    avatar: './img/avatar_2.jpg',
                     visible: true,
                     messages: 
                     [
@@ -59,7 +59,7 @@ createApp ({
 
                 {
                     name: 'Samuele',
-                    avatar: './img/avatar_3.png',
+                    avatar: './img/avatar_3.jpg',
                     visible: true,
                     messages: 
                     [
@@ -85,7 +85,7 @@ createApp ({
 
                 {
                     name: 'Alessandro B.',
-                    avatar: './img/avatar_4.png',
+                    avatar: './img/avatar_4.jpg',
                     visible: true,
                     messages: 
                     [
@@ -105,7 +105,7 @@ createApp ({
 
                 {
                     name: 'Alessandro L.',
-                    avatar: './img/avatar_5.png',
+                    avatar: './img/avatar_5.jpg',
                     visible: true,
                     messages: 
                     [
@@ -125,7 +125,7 @@ createApp ({
 
                 {
                     name: 'Claudia',
-                    avatar: './img/avatar_5.png',
+                    avatar: './img/avatar_5.jpg',
                     visible: true,
                     messages: 
                     [
@@ -151,7 +151,7 @@ createApp ({
 
                 {
                     name: 'Federico',
-                    avatar: './img/avatar_7.png',
+                    avatar: './img/avatar_7.jpg',
                     visible: true,
                     messages: 
                     [
@@ -171,7 +171,7 @@ createApp ({
 
                 {
                     name: 'Davide',
-                    avatar: './img/avatar_8.png',
+                    avatar: './img/avatar_8.jpg',
                     visible: true,
                     messages: 
                     [
